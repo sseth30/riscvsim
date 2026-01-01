12420cb09a9bc32a2cce8086f30e8c162ace21f5
Satchit Seth <satchitseth@Satchits-M4Pro-MacBook-Pro.local>
2026-01-01 18:29:54 -0500

    bugfixing with example/

--- amplify/backend/function/riscvsimBackend/src/main/java/example/ResponseClass.java
+++ amplify/backend/function/riscvsimBackend/src/main/java/example/ResponseClass.java
@@ -1,19 +1,35 @@
-
-
 package example;
-        
-     public class ResponseClass {
-        String greetings;
 
-        public String getGreetings() {
-            return this.greetings;
-        }
+/**
+ * Simple response payload wrapping a greeting string.
+ */
+public class ResponseClass {
+    private String greetings;
 
-        public void setGreetings(String greetings) {
-            this.greetings = greetings;
-        }
-
-        public ResponseClass(String greetings) {
-            this.greetings = greetings;
-        }
+    /**
+     * Returns the greeting text.
+     *
+     * @return greeting value
+     */
+    public String getGreetings() {
+        return this.greetings;
     }
+
+    /**
+     * Sets the greeting text.
+     *
+     * @param greetings greeting value
+     */
+    public void setGreetings(String greetings) {
+        this.greetings = greetings;
+    }
+
+    /**
+     * Constructs a populated response.
+     *
+     * @param greetings greeting value
+     */
+    public ResponseClass(String greetings) {
+        this.greetings = greetings;
+    }
+}
